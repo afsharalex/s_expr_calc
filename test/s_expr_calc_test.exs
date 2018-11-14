16e08b98ca10ca45b1cfa20b6c09d59ba7f75399
Alex Afshar <contact@alexafshar.com>
2018-11-13 19:38:06 -0500

    Added some tests for SExprCalc.parse/1.

--- test/s_expr_calc_test.exs
+++ test/s_expr_calc_test.exs
@@ -2,7 +2,32 @@ defmodule SExprCalcTest do
   use ExUnit.Case
   doctest SExprCalc
 
-  test "greets the world" do
-    assert SExprCalc.hello() == :world
+  describe "parse/1" do
+    test "simple addition s-expression" do
+      program = "(add 1 1)"
+      expected = [:+, 1, 1]
+      result = SExprCalc.parse(program)
+
+      assert expected -- result == []
+      assert result -- expected == []
+    end
+
+    test "simple multiplication s-expression" do
+      program = "(multiply 2 3)"
+      expected = [:*, 2, 3]
+      result = SExprCalc.parse(program)
+
+      assert expected -- result == []
+      assert result -- expected == []
+    end
+
+    test "complex addition and multiplication s-expression" do
+      program = "(add (add 1 1) (multiply 2 2))"
+      expected = [:+, :+, 1, 1, :*, 2, 2]
+      result = SExprCalc.parse(program)
+
+      assert expected -- result == []
+      assert result -- expected == []
+    end
   end
 end
